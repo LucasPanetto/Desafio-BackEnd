7b4fec587138c82f4e95a97be86e5cbf670d3a5b
unknown <lucaspanetto@ucl.br>
2025-09-16 17:19:44 -0300

    Feat: Criada estrutura de pastas

---
+++ MotorcycleRental/MotorcycleRental.Api/Controllers/RentalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotorcycleRental.Api.Controllers
+{
+    public class RentalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
