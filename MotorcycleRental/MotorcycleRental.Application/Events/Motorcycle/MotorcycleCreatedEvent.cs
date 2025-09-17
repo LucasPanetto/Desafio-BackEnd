328356b2db4e873bb672294c08230ed89507b22f
LucasPanetto <lucaspanetto@ucl.br>
2025-09-17 19:05:05 -0300

    [ Feat ] - Criado evento para cadastro de moto

---
+++ MotorcycleRental/MotorcycleRental.Application/Events/Motorcycle/MotorcycleCreatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace MotorcycleRental.Application.Events.Motorcycle
+{
+    public class MotorcycleCreatedEvent : INotification
+    {
+        public int InternalId { get; }
+        public string Id { get; }
+        public string Plate { get; }
+        public string Model { get; }
+        public int Year { get; }
+
+        public MotorcycleCreatedEvent(int internalId, string id, string plate, string model, int year)
+        {
+            InternalId = internalId;
+            Id = id;
+            Plate = plate;
+            Model = model;
+            Year = year;
+        }
+    }
+}
