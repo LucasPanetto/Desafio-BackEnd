c2244e2f348e42c6498e70a119a9978923c09c09
LucasPanetto <lucaspanetto@ucl.br>
2025-09-17 19:00:58 -0300

    [ Feat ] - Configurado program.cs e AppSettings

--- MotorcycleRental/MotorcycleRental.Api/Program.cs
+++ MotorcycleRental/MotorcycleRental.Api/Program.cs
@@ -1,15 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using MotorcycleRental.Api.Middlewares;
+using MotorcycleRental.Application.Handlers.DeliveryMan;
+using MotorcycleRental.Infrastructure.Interfaces;
+using MotorcycleRental.Infrastructure.Messaging;
+using MotorcycleRental.Infrastructure.Persistence;
+using MotorcycleRental.Infrastructure.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adiciona controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlPath = Path.Combine(AppContext.BaseDirectory,
+        $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+    c.IncludeXmlComments(xmlPath);
+});
+
+// DbContext
+builder.Services.AddDbContext<MotorcycleRentalDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositórios
+builder.Services.AddScoped<IMotorcycleRepository, MotorcycleRepository>();
+builder.Services.AddScoped<IDeliveryManRepository, DeliveryManRepository>();
+builder.Services.AddScoped<IRentalRepository, RentalRepository>();
+builder.Services.AddScoped<IPlanRepository, PlanRepository>();
+
+// MediatR
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+    typeof(CreateRentalHandler).Assembly
+));
+
+builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection("RabbitMQ"));
+builder.Services.AddScoped<IMessageBus, RabbitMqMessageBus>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Aplica migrations automaticamente
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MotorcycleRentalDbContext>();
+    db.Database.Migrate();
+}
+
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +56,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
